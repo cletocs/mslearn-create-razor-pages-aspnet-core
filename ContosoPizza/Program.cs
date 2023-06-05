2c220163c34b99d8958f1b5914bce7daf8a40441
cletocs <cletocs@gmail.com>
2023-06-05 19:07:00 +0000

    appworks1

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -6,10 +6,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<PizzaContext>(options =>
     options.UseSqlite("Data Source=ContosoPizza.db"));
-
+builder.Services.AddScoped<PizzaService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
